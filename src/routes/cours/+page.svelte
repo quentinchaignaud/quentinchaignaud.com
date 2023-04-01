8034d1cf35bb2275ed9683f01f73a03be7e157c3
QuentinJS1 <contact@quentinjs.com>
2023-04-01 17:13:11 +0200

    :bug: fix badges

--- src/routes/cours/+page.svelte
+++ src/routes/cours/+page.svelte
@@ -23,24 +23,25 @@
         <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl dark:text-white">Cours</h2>
         <p class="mt-2 text-base leading-8 text-gray-600 dark:text-gray-300">Tout les cours présents ici sont 100% gratuits. Ils se concentrent sur des concepts plutôt que des langages, pour que les juniors puissent obtenir cette vision d'ensemble qui leur fait souvent défaut.</p>
         <div class="mt-16 space-y-20 lg:mt-20 lg:space-y-20">
-          <article class="relative isolate flex flex-col gap-8 lg:flex-row lg:hover:bg-gray-100 p-4 rounded-xl hover:cursor-pointer dark:lg:hover:bg-gray-800">
+          <article class="relative isolate flex flex-col gap-8 lg:flex-row lg:hover:bg-gray-100 rounded-xl hover:cursor-pointer dark:lg:hover:bg-gray-800">
             <div class="relative aspect-[16/9] sm:aspect-[2/1] lg:aspect-square lg:w-64 lg:shrink-0">
-              <img src={imgData} alt="Illustration d'un casier contenant des dossiers." class="absolute inset-0 h-full w-full rounded-2xl bg-white dark:bg-gray-700 object-cover">
+              <img src={imgData} alt="Illustration d'un casier contenant des dossiers." class="absolute inset-0 h-full w-full rounded-2xl bg-white dark:bg-gray-700 object-contain lg:object-cover">
               <div class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10"></div>
             </div>
             <div>
               <div class="group relative max-w-xl">
-                <div class="lg:mt-3 flex items-center justify-between">
-                    <h3 class="text-xl font-semibold leading-6 text-gray-900 dark:text-white">
+                <div class="lg:mt-3 flex flex-col-reverse md:flex-row md:items-center md:justify-between">
+                    <h3 class="mt-3 text-xl font-semibold leading-6 text-gray-900 dark:text-white">
                       <a href="/cours/comprendre-les-bases-de-donnees">
                         <span class="absolute inset-0"></span>
                         Comprendre les bases de données
                       </a>
                     </h3>
-                    <span class="inline-flex items-center rounded-full bg-purple-100 px-2.5 py-0.5 text-xs font-medium text-purple-800 dark:text-indigo-400 dark:bg-indigo-500/10 dark:ring-1 dark:ring-inset dark:ring-indigo-500/20">En chantier</span>
+                    <span class="w-24 inline-flex items-center justify-center rounded-full bg-purple-100 px-2.5 py-0.5 text-xs font-medium text-purple-800 dark:text-indigo-400 dark:bg-indigo-500/10 dark:ring-1 dark:ring-inset dark:ring-indigo-500/20">En chantier</span>
                 </div>
                 <div class="mt-5 text-sm leading-6 text-gray-600 dark:text-gray-300">
-                  Ce cours est le seul dont vous aurez besoin pour comprendre en profondeur comment marchent les bases de données. Conçu pour les débutants qui souhaiteraient passer à un niveau avancé.
+                  Ce cours est le seul dont vous aurez besoin pour comprendre en profondeur comment marchent les bases de données. Conçu pour les débutants qui souhaitent passer à un niveau avancé.
+                  A la fin, vous saurez :
                     <ul class="mt-2 max-w-xl space-y-2">
                         <li class="flex gap-x-2 justify-start">
                           <svg class="mt-0.5 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
