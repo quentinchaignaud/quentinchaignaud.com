0c19e3983235637f224f1cef72986a2ae8f51e20
quentinchaignaud <quentinchaignaud@gmail.com>
2023-07-22 11:23:30 +0200

    feat: add legal mentions and read me pictures

---
+++ src/routes/legal/mentions-legales/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    let nomDuSite = 'quentinchaignaud.com'
+</script>
+
+<div class="py-32 px-6 lg:px-8">
+    <div class="mx-auto max-w-3xl text-base leading-7 text-gray-700">
+      <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl dark:text-white">Mentions légales</h1>
+      <div class="mt-10 max-w-2xl dark:text-gray-300">
+        <p class="mt-6">Le site web {nomDuSite} est édité par la micro-entreprise Quentin Chaignaud, immatriculée au registre du commerce et des sociétés sous le numéro 917397762.</p>
+        <p class="mt-6">Le siège social de l'entreprise est situé à 61 Rue de Lyon, 75012 Paris.</p>
+        <p class="mt-6">Le directeur de la publication du site est M. Quentin Chaignaud.</p>
+        <p class="mt-6">Le site est hébergé par la société Vercel, dont le siège social est situé à Vercel Inc., 340 S Lemon Ave #4133, Walnut, CA 91789.</p>
+        <p class="mt-6">Pour toute question ou demande concernant le site, vous pouvez nous contacter à l'adresse e-mail suivante: quentinchaignaud@gmail.com</p>
+        <p class="mt-6">Conformément à la loi française sur la protection des données personnelles (Loi n°78-17 du 6 janvier 1978), vous disposez d'un droit d'accès, de modification, de rectification et de suppression des données vous concernant. Pour exercer ce droit, veuillez nous contacter à l'adresse e-mail mentionnée ci-dessus.</p>
+        <p class="mt-6">Le site {nomDuSite} est soumis au droit français. En cas de litige, les tribunaux français seront compétents.</p>
+        <p class="mt-6">La création du site a été réalisée par la société Quentin Chaignaud et son contenu est la propriété exclusive de Quentin Chaignaud. La reproduction de tout ou partie de ce site sur un support quel qu'il soit est interdite sans l'autorisation expresse et préalable de Quentin Chaignaud.</p>
+        <p class="mt-6">Les marques et logos présents sur ce site sont la propriété de Quentin Chaignaud ou de tiers ayant autorisé Quentin Chaignaud à les utiliser. Toute utilisation de ces marques et logos sans l'autorisation expresse de leurs propriétaires est interdite.</p>
+        <p class="mt-6">Le site {nomDuSite} peut contenir des liens vers d'autres sites web qui ne sont pas sous le contrôle de Quentin Chaignaud. Quentin Chaignaud ne peut être tenu responsable du contenu de ces sites et de l'utilisation qui pourrait en être faite.</p>
+        <p class="mt-6">Ces mentions légales ont été mises à jour le 25 février 2023.</p>
+      </div>
+    </div>
+  </div>
+  
